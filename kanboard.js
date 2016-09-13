33a50199aa8e96481d57fe544ee8fcb4d221334f
Marc-Antoine Bouchard Marceau <marc-antoine.bm@libeo.com>
2016-09-13 08:27:05 -0400

    First commit

---
+++ kanboard.js
@@ -0,0 +1,22 @@
+==UserScript==
+// @name         Kanboard
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Marc-Antoine BM
+// @match        https://kanban.libeo.com/board/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('.task-board-age-total').each(function(key, item) {
+        $(item).css('font-size', $(item).html().replace('d', '') * 2 + 'px');
+    });
+
+    $('.task-board-title').each(function(key, item) {
+        var value = $($(item).find('a')[0]).html();
+        $(item).html(value);
+    });
+})();
